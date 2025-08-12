2b2237b1ffe1b3add7fcd60e8901830cf9d8d6e8
mintan225 <mintanyeo997@gmail.com>
2025-08-12 11:23:22 +0000

    Initial commit of the project

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -3,12 +3,17 @@ import { config } from 'dotenv';
 
 config();
 
+// Condition pour vérifier si l'on est sur Render et activer SSL
+const isRender = process.env.DATABASE_URL?.includes('render.com');
+
 export default defineConfig({
   dialect: 'postgresql',
   schema: '../shared-types/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    url: process.env.DATABASE_URL!,
+    // Ajoute la configuration SSL ici
+    ...(isRender && { ssl: { rejectUnauthorized: false } }),
   },
   verbose: true,
   strict: true
